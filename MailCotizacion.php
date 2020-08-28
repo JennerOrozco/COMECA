9535fccb6e51a4c6256d306b9da3043af075cd15
Jenner Fernando Orozco <jf.orozco3@gmail.com>
2020-08-28 14:15:42 -0600

    COMMIT CASI FINALIZADO

---
+++ MailCotizacion.php
@@ -0,0 +1,15 @@
+<?php 
+    $to = "comeca@comeca.com"; // this is your Email address
+    $from = $_POST['Mail']; // this is the sender's Email address
+    $first_name = $_POST['Name'];    
+    $subject = "COTIZACION WEB " . $_POST['Producto'] . " ";        
+    $message = "*****  DATOS CLIENTE ***** ".  "\n\n" .  " Nombre: " . $first_name ."\n\n" . " TEL: " . $_POST['Tel'] ."\n\n" . " PRODUCTO PARA COTIZAR: " . $_POST['Producto'] ."\n\n" . " CONTENIDO DEL MENSAJE :" . "\n\n" . $_POST['Mensaje'];
+    $message2 = "Nuestro equipo ah recibido su mensaje Sr/Sra. " . $first_name . "\n\n" . $_POST['Mensaje'] . "\n\n" . "Nos comunicaremos con usted lo antes posible.";
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject,$message2,$headers2);
+    
+    
+?>
